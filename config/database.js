98435a6c147cbc48ab30da1d16251aad38ed5304
yushaliniepillay <yushalinie97@hotmail.com>
2021-02-02 18:24:45 +0800

    database

--- config/database.js
+++ config/database.js
@@ -1,5 +1,3 @@
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
 
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
@@ -8,12 +6,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi-app'),
-        username: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', 'admin'),
-        ssl: env.bool('DATABASE_SSL', false),
+        host: "${process.env.DATABASE_HOST || 'localhost'}",
+        port: "${process.env.DATABASE_PORT || 5432}",
+        database: "${process.env.DATABASE_NAME || 'strapi-app}",
+        username: "${process.env.DATABASE_USERNAME || 'postgres}",
+        password: "${process.env.DATABASE_PASSWORD || 'admin}",
       },
       options: {}
     },
