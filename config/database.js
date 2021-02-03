47d1559fde568b9e602c83eef66b9d1081b47f3b
yushaliniepillay <yushalinie97@hotmail.com>
2021-02-03 15:41:33 +0800

    update database config

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: "${process.env.DATABASE_HOST || 'localhost' }",
+        host: "${process.env.DATABASE_HOST|| 'localhost' }",
         port: "${process.env.DATABASE_PORT || 5432 }",
         database: "${process.env.DATABASE_NAME || 'strapi-app' }",
         username: "${process.env.DATABASE_USERNAME || 'postgres' }",
