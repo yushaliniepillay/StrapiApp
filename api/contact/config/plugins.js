f1d5f664c98141485ce7a94b84db433d96ef83ea
yushaliniepillay <yushalinie97@hotmail.com>
2021-04-22 13:55:26 +0800

    add controller for email service

---
+++ api/contact/config/plugins.js
@@ -0,0 +1,10 @@
+module.exports = {
+    
+      provider: 'sendgrid',
+      providerOptions: {},
+      settings: {
+        defaultFrom: 'no-reply@strapi.io',
+        
+      },
+    
+  };
--- api/contact/controllers/contact.js
+++ api/contact/controllers/contact.js
@@ -5,4 +5,38 @@
  * to customize this controller
  */
 
-module.exports = {};
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+
+
+module.exports = {
+
+    async create(ctx) {
+        let entity;
+        if (ctx.is('multipart')) {
+            const { data, files } = parseMultipartData(ctx);
+            entity = await strapi.services.contact.create(data, { files });
+        } else {
+            entity = await strapi.services.contact.create(ctx.request.body);
+        }
+        entity = sanitizeEntity(entity, { model: strapi.models.contact });
+
+        const ORMModel = await strapi.query('contact', plugin).model;
+        const newCustomEntry = await strapi.plugins['email'].services.email.send({
+                to: 'yusha.b@avowstech.com',
+                from: contact.email,
+                replyTo: 'no-reply@strapi.io',
+                subject: `A new message from Avows contact form, subject: ${contact.services}`,
+                text: contact.message,
+                html: contact.message,
+            });
+            strapi.log.debug(`Email sent to ${contact.email}`)
+            ctx.send({ message: "Email sent!" })
+            
+
+        ORMModel.lifecycles.afterCreate(newCustomEntry.toJSON());
+        
+        
+        // return sanitizeEntity(entity, { model: strapi.models.contact });
+    },
+
+};
