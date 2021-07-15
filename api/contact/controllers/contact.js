4cc98fe2cd2414d8be97c96f7652a25fccfe3d9e
yushaliniepillay <yushalinie97@hotmail.com>
2021-07-15 20:45:32 +0800

    debug auto email notify

--- api/contact/controllers/contact.js
+++ api/contact/controllers/contact.js
@@ -4,69 +4,20 @@
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
- const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+//  const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 
 
  module.exports = {
-  async create(ctx) {
-            strapi.services.sms.sendEmail();
-            let entity;
-            if (ctx.is('multipart')) {
-              const { data, files } = parseMultipartData(ctx);
-              entity = await strapi.services.contact.create(data, { files });
-            } else {
-              entity = await strapi.services.contact.create(ctx.request.body);
-            }
-            return sanitizeEntity(entity, { model: strapi.models.contact });
-
-    //  async create(ctx) {
-    //     let entity;
-    //     const userEmail = process.env.EMAIL;
-    //     const userPass = process.env.PASSWORD;
-
-    //      if (ctx.is('multipart')) {
-    //          const { data, files } = parseMultipartData(ctx);
-    //          entity = await strapi.services.contact.create(data, { files });
-    //      } else {
-    //          entity = await strapi.services.contact.create(ctx.request.body);
-    //      }
-    //      entity = sanitizeEntity(entity, { model: strapi.models.contact });
- 
-    //      const nodemailer = require('nodemailer');
- 
-    //      const transporter = nodemailer.createTransport({
-    //          // service: 'smtp',
-    //          host: 'mail.avowstech.com',
-    //          port: 465,
-    //          secure: true,
-    //          auth: {
-    //              user: userEmail,
-    //              pass: userPass // naturally, replace both with your real credentials or an application-specific password
-    //          }
-    //      });
- 
-    //      const ORMModel = await strapi.query('contact', plugin).model;
-    //      const mailOptions = await strapi.plugins['email'].services.email.send({
-    //          from: `${contact.email}`,
-    //          to: 'yusha.b@avowstech.com',
-    //          subject: `${contact.services}`,
-    //          text: `${contact.message}`
-    //      });
-    //      strapi.log.debug(`Email sent to ${contact.email}`)
-    //      ctx.send({ message: "Email sent!" })
- 
-    //      transporter.sendMail(mailOptions, function (error, info) {
-    //          if (error) {
-    //              console.log(error);
-    //          } else {
-    //              console.log('Email sent: ' + info.response);
-    //          }
-    //      });
- 
-    //      ORMModel.lifecycles.afterCreate(newCustomEntry.toJSON());
-    //      return sanitizeEntity(entity, { model: strapi.models.contact });
- 
-        
-     },
+//   async create(ctx) {
+//             strapi.services.sms.sendEmail();
+//             let entity;
+//             if (ctx.is('multipart')) {
+//               const { data, files } = parseMultipartData(ctx);
+//               entity = await strapi.services.contact.create(data, { files });
+//             } else {
+//               entity = await strapi.services.contact.create(ctx.request.body);
+//             }
+//             return sanitizeEntity(entity, { model: strapi.models.contact });
+//      },
  
  };
--- api/contact/services/contact.js
+++ api/contact/services/contact.js
@@ -5,59 +5,59 @@
  * to customize this service
  */
 
- const userEmail = process.env.EMAIL;
- const userPass = process.env.PASSWORD;
+//  const userEmail = process.env.EMAIL;
+//  const userPass = process.env.PASSWORD;
  
- let sendEmail = function(){
-      const nodemailer = require('nodemailer');
-      const transporter = nodemailer.createTransport({
-          // service: 'smtp',
-          host: 'mail.avowstech.com',
-          port: 465,
-          secure: true,
-          auth: {
-              user: userEmail,
-              pass: userPass // naturally, replace both with your real credentials or an application-specific password
-          }
-          .then(message => console.log(message.sid))
-      });
+//  let sendEmail = function(){
+//       const nodemailer = require('nodemailer');
+//       const transporter = nodemailer.createTransport({
+//           // service: 'smtp',
+//           host: 'mail.avowstech.com',
+//           port: 465,
+//           secure: true,
+//           auth: {
+//               user: userEmail,
+//               pass: userPass // naturally, replace both with your real credentials or an application-specific password
+//           }
+//           .then(message => console.log(message.sid))
+//       });
 
-        const ORMModel = await strapi.query('contact', plugin).model;
-        const mailOptions = await strapi.plugins['email'].services.email.send({
-             from: `${contact.email}`,
-             to: 'yusha.b@avowstech.com',
-             subject: `${contact.services}`,
-             text: `${contact.message}`
-         });
-         strapi.log.debug(`Email sent to ${contact.email}`)
-         ctx.send({ message: "Email sent!" })
+//         const ORMModel = await strapi.query('contact', plugin).model;
+//         const mailOptions = await strapi.plugins['email'].services.email.send({
+//              from: `${contact.email}`,
+//              to: 'yusha.b@avowstech.com',
+//              subject: `${contact.services}`,
+//              text: `${contact.message}`
+//          });
+//          strapi.log.debug(`Email sent to ${contact.email}`)
+//          ctx.send({ message: "Email sent!" })
  
-         transporter.sendMail(mailOptions, function (error, info) {
-             if (error) {
-                 console.log(error);
-             } else {
-                 console.log('Email sent: ' + info.response);
-             }
-         });
-    }
+//          transporter.sendMail(mailOptions, function (error, info) {
+//              if (error) {
+//                  console.log(error);
+//              } else {
+//                  console.log('Email sent: ' + info.response);
+//              }
+//          });
+//     }
 
- let sendMsg = function(){
-    const accountSid = process.env.TWILIO_ACCOUNT_SID ;
-    const authToken = process.env.TWILIO_AUTH_TOKEN  ;
-    const myNumi = process.env.MYNUM;
-    const twilioNum =process.env.TWILIONUM;
+//  let sendMsg = function(){
+//     const accountSid = process.env.TWILIO_ACCOUNT_SID ;
+//     const authToken = process.env.TWILIO_AUTH_TOKEN  ;
+//     const myNumi = process.env.MYNUM;
+//     const twilioNum =process.env.TWILIONUM;
     
-    const client = require('twilio')(accountSid, authToken);
-    client.messages
-      .create({
-         body: 'Hello Admin, someone just posted a comment',
-         from: 'mail.avowstech.com', 
-         to: userEmail // your own phone number
-       })
-      .then(message => console.log(message.sid));
-   }
+//     const client = require('twilio')(accountSid, authToken);
+//     client.messages
+//       .create({
+//          body: 'Hello Admin, someone just posted a comment',
+//          from: 'mail.avowstech.com', 
+//          to: userEmail // your own phone number
+//        })
+//       .then(message => console.log(message.sid));
+//    }
 
 module.exports = {
-    sendEmail,
-    sendMsg
+    // sendEmail,
+    // sendMsg
 };
